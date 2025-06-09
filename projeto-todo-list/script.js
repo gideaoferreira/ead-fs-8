acb76dde963698190c4a84b9594c40759741173f
Gideão Ferreira <gideaoferreira@hotmail.com>
2025-06-09 18:45:04 -0300

    Aula dia 04-06-2025

--- projeto-todo-list/script.js
+++ projeto-todo-list/script.js
@@ -1,115 +1,18 @@
-// O que é DOM?
-// Document Object Model (DOM) é uma interface de
-// programação para documentos HTML e XML.
-// Ele representa a estrutura do documento como
-// uma árvore de objetos, onde cada nó da árvore
-// corresponde a uma parte do documento, como elementos,
-// atributos e texto. O DOM permite que linguagens de
-// programação, como JavaScript, acessem e manipulem
-// conteúdo, a estrutura e o estilo dos documentos.
-
-// document
-// é um objeto global que representa o documento HTML
-
-// document.querySelector
-// é um método que retorna o primeiro elemento
-// que corresponde ao seletor CSS/HTML fornecido.
-
-// getElementById()
-// Retorna o elemento com o ID especificado.
-// const titleElement = document.getElementById('title');
-
-// getElementsByClassName()
-// Retorna uma coleção de elementos com a classe especificada.
-// const paragraphs = document.getElementsByClassName('paragraph');
-
-// getElementsByTagName()
-// Retorna uma coleção de elementos com a tag especificada.
-// const listItems = document.getElementsByTagName('input');
-// Usando o elemento attribute para manipular atributos
-// const inputName = listItems[0]
-// inputName.getAttribute('class')
-// listItems[0].setAttribute('placeholder', 'Novo Placeholder');
-// listItems[0].removeAttribute('disabled');
-
-
-// querySelector()
-// Retorna o primeiro elemento que corresponde ao seletor CSS/HTML fornecido.
-// const title = document.querySelector('#title');
-// title.innerText = 'DOM - Document Object Model - Modificado';
-
-// querySelectorAll()
-// Retorna uma lista de todos os elementos que correspondem ao seletor CSS/HTML fornecido.
-// const allParagraphs = document.querySelectorAll('.paragraph');
-
-{/* <a href="">Link</a>	 */ }
-// <img src="../caminho/da/imagem.png" alt="Texto alternativo"></img>
-{/* <input
-    name="name"
-    type="button"
-    class="input"
-    id="input-name"
-    disabled
-></input> */}
-
-// input é um elemento HTML
-// Todo elemento HTML tem atributos
-
-// document.getElementById
-// Pega o elemento HTML com o ID especificado
 const btnCollapseLayout = document.getElementById('collpse-layout');
 
-// addEventListener
-// Adiciona um evento ao elemento HTML
 btnCollapseLayout.addEventListener('click', () => {
-    // Pego o elemento HTML com a tag 'nav'
     const navMenu = document.getElementsByTagName('nav');
-    // Extraio o primeiro elemento da coleção/"array"
     const nav = navMenu[0];
-    
-    // Pego o elemento HTML com a tag 'header'
+
     const headerEle = document.getElementsByTagName('header');
-    // Extraio o primeiro elemento da coleção/"array"
     const header = headerEle[0];
 
-    // Pego o elemento HTML com a tag 'main'
     const mainEle = document.getElementsByTagName('main');
-    // Extraio o primeiro elemento da coleção/"array"
     const main = mainEle[0];
 
-    // Forma 1 de adicionar/remover uma classe
-    // if (nav.classList.contains('collapse')) {
-    //     btnCollapseLayout.innerText = '<<';
-    //     nav.removeAttribute('class');
-    //     header.removeAttribute('class');
-    //     main.removeAttribute('class');
-    //     return;
-    // }
-    // if (!nav.classList.contains('collapse')) {
-    //     btnCollapseLayout.innerText = '>>';
-    //     nav.setAttribute('class', 'collapse');
-    //     header.setAttribute('class', 'collapse');
-    //     main.setAttribute('class', 'collapse');
-    // }
-
-    // Forma 2 de adicionar/remover uma classe
-    // if (nav.classList.contains('collapse')) {
-    //     btnCollapseLayout.innerText = '<<';
-    //     nav.classList.remove('collapse');
-    //     header.classList.remove('collapse');
-    //     main.classList.remove('collapse');
-    //     return;
-    // }
-    // if (!nav.classList.contains('collapse')) {
-    //     btnCollapseLayout.innerText = '>>';
-    //     nav.classList.add('collapse');
-    //     header.classList.add('collapse');
-    //     main.classList.add('collapse');
-    // }
-
-    nav.classList.toggle('collapse');
-    header.classList.toggle('collapse');
-    main.classList.toggle('collapse');
+    nav.classList.toggle('app-collapse');
+    header.classList.toggle('app-collapse');
+    main.classList.toggle('app-collapse');
 
     if (nav.classList.contains('collapse')) {
         btnCollapseLayout.innerText = '>>';
@@ -120,7 +23,59 @@ btnCollapseLayout.addEventListener('click', () => {
     }
 })
 
-const paragraphsEle = document.getElementsByTagName('p');
-const paragraphs = paragraphsEle[0]
-console.log(paragraphs.classList.remove('description'));
-console.log(paragraphs.classList);
+const btnModalTodo = document.getElementById('btn-modal-todo');
+
+if (btnModalTodo) {
+    btnModalTodo.addEventListener('click', () => {
+        const todoForm = document.getElementById('todo-form');
+        const dataForm = new FormData(todoForm);
+        const dataTask = Object.fromEntries(dataForm);
+        
+        const tableBody = document.getElementById('table-body');
+    
+        let statusTask = 'Concluído'; 
+        if (!dataTask.status) {
+            statusTask = 'Em andamento';
+        }
+    
+        tableBody.innerHTML +=`
+            <tr>
+                <td>${dataTask.task}</td>
+                <td>${statusTask}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm">Excluir</button>
+                    <button class="btn btn-success btn-sm" ${dataTask.status === 'on' ? 'disabled' : ''}>Concluir</button>
+                </td>
+            </tr>
+        `
+        console.log(dataTask);
+    }); 
+}
+
+const btnAddUser = document.getElementById('add-user')
+
+if (btnAddUser) {
+    btnAddUser.addEventListener('click', () => {
+        const userForm = document.getElementById('user-form')
+        const formData = new FormData(userForm)
+        const userData = Object.fromEntries(formData)
+
+        let tableBody = document.getElementById('table-body')
+
+        if (userData.name && userData.lastname && userData.email && userData.phoneNumber) {
+            tableBody.innerHTML += `
+                <tr>
+                    <td>${userData.name}</td>
+                    <td>${userData.lastname}</td>
+                    <td>${userData.email}</td>
+                    <td>${userData.phoneNumber}</td>
+                    <td>
+                        <button class="btn btn-danger btn-sm">Excluir</button>
+                        <button class="btn btn-success btn-sm">Editar</button>
+                        <button class="btn btn-primary btn-sm">Detalhes</button>
+                    </td>
+                </tr>
+            `
+        }
+    })
+}
